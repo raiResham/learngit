8a5530e2a251b716c133f037f45b90a133468261
Resham Rai <resham.bayung.rai@gmail.com>
2019-12-19 16:40:25 +0545

    Added git tips and tricks

--- learngit.js
+++ learngit.js
@@ -1,2 +1,32 @@
 write some js code here!
 document.queryselector()
+
+
+
+----------
+
+Initialize empty git repository
+- <go inside folder>$git init
+
+
+Create a branch
+	- Why branch?
+		- Add experimental feature
+		- If satisfied merge back
+	- Create a branch
+		-$git branch branchname	
+		IMP: Now push the branch to origin (Otherwise github will not have new branch)
+		git push origin my_new_branch
+
+List all branches
+	- git branch --list
+
+
+Switch between branches
+------------------------
+	$ git checkout branch_name	
+
+-------------------
+$git add learngit.js
+	- It adds learngit.js to staging area!
+	- Now $git diff wont work instead try gi diff --staged
